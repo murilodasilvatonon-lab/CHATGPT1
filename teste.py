725c0b5686dd5548de87e479e3dca4503a4bc962
murilodasilvatonon-lab <murilodasilvatonon@gmail.com>
2026-05-16 11:37:31 -0300

    ignorando info pessoal

--- teste.py
+++ teste.py
@@ -1,11 +1,26 @@
 import requests
 
+#colhe o cep
+nome = input("Digite seu nome: ")
+email = input("Digite seu e-mail: ")
+telefone = input("Digite seu telefone: ")
 cep = input("Qual seu cep? ")
 
+#cria uma variael e atribui o resultado do link
+#Utilizamos o f string, para conseguir inserir uma variavel
 url = f"https://viacep.com.br/ws/{cep}/json/"
 
-dados1 = requests.get(url)
+dados = requests.get(url).json()
 
-dadosfinal = dados1.json()
+print(f"Bem vindo ao Mercado Libre {nome}! o seu e-mail é {email}. O seu telefone é {telefone}. você mora na rua {dados['logradouro']}, na cidade {dados['localidade']}, no estado de {dados['estado']}.") 
 
-print(dadosfinal)
+#atribuindo variaveis para cada um dos resultados
+# rua = dados['logradouro']
+# bairro = dados['bairro']
+# cidade = dados['localidade']
+
+# bairro = dados
+
+# print(rua)
+# print(bairro)
+# print(cidade)
